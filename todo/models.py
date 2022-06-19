78dc5c6a721ab6e4b26d0f0d32f92d040e293a95
mikakallberg <mikaela.kallberg@hotmail.com>
2022-06-19 12:59:34 +0000

    Add edit_item toggle_item and delete_item

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class Item(models.Model):
+    """
+    Sets instructions to admin page
+    """
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Item
+from .forms import Itemform
 
 
 # Create your views here.
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    """
+    Render todo list
+    """
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    """
+    Render todo list
+    """
+    if request.method == 'POST':
+        form = Itemform(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect('get_todo_list')
+    form = Itemform()
+    context = {
+            'form': form
+        }
+
+    return render(request, 'todo/add_list.html', context)
+
+
+def edit_item(request, item_id):
+    """
+    Render edit item
+    """
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        form = Itemform(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+        return redirect('get_todo_list')
+
+    form = Itemform(instance=item)
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/edit_item.html', context)
+
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+
+    return redirect('get_todo_list')
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_list')
